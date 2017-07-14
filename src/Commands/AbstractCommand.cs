2d4d01b7238cb15df46d3ec2f5f4dfb20620862c
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2017-07-14 19:02:46 +0200

    Added abstract command.

---
+++ src/Commands/AbstractCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KarveCar.Commands
+{
+    public abstract class  AbstractCommand: ICommand
+
+    {
+        public event EventHandler CanExecuteChanged;
+        public abstract bool CanExecute(object parameter);
+        public abstract void Execute(object parameter);
+    }
+}
