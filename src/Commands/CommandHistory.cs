e8d11eba834396a3aaa8efc259015d0e49305885
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2017-07-21 12:22:31 +0200

    Adding command history for the application.

--- src/Commands/CommandHistory.cs
+++ src/Commands/CommandHistory.cs
@@ -1,182 +1,201 @@
-﻿using System;
+﻿/**
+ * Copyright 2017 KarveInformatica S.L.
+ * CommandHistory module.
+ * author Giorgio Zoppi <giorgio.zoppi@karveinformatica.com>
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+
 
 namespace KarveCar.Commands
 {
+    /// <summary>
+    ///  CommandWrapper. It wraps the command for placing in the parameters.
+    /// </summary>
+    public class CommandWrapper
+    {
+       // Command
+        private AbstractCommand _command;
+        // Parameters of the command.
+        private object _parameters;
+
+        public CommandWrapper(AbstractCommand cmd, object param)
+        {
+            this.Parameters = param;
+            this.Command = cmd;
+        }
+
+        public object Parameters { get => _parameters; set => _parameters = value; }
+        public AbstractCommand Command { get => _command; set => _command = value; }
+        
+    }
+    /// <summary>
+    ///  CommandHistory. It take trace of each command fired in the application and it allows the redo/undo.
+    ///  Singleton Pattern.
+    /// </summary>
     public class CommandHistory
     {
+        // List of the redo command
+        private IList<CommandWrapper> redoList;
+        // List of the undo command
+        private IList<CommandWrapper> undoList;
+        // List of the history list
+        private IList<CommandWrapper> historyList;
+        // single instance of the command list.
+        private static CommandHistory _instance;
+        // This is useful to enable or disable the Undo. In case is different from zero there are some command in the lists.
+        private int _cleanUpCount;
 
-
-        /*
-         
-          void CommandHistory::AddHistory(Command* c)
+        /// <summary>
+        ///  Constructor of the CommandHistory
+        ///  It shall be private because it is a singleton.
+        /// </summary>
+        private CommandHistory()
         {
-            if (_history.size() > MAX_UNDO_COMMAND * 2)
-                ClearList(&_history);
-            _history.push_back(c);
+            redoList = new List<CommandWrapper>();
+            undoList = new List<CommandWrapper>();
+            _cleanUpCount = 0;
         }
-        void CommandHistory::Repeat()
+       /// <summary>
+       ///  Singleton accessory method.
+       /// </summary>
+       /// <returns> The current command history.</returns>
+        public static CommandHistory GetInstance()
         {
-            command_iterator it;
-            CommandList tmpList;
-            ClearList(&_redoList);
-            ClearList(&_undoList);
-            std::copy(_history.begin(), _history.end() - 1, back_inserter(tmpList));
-            _history.clear();
-            for (it = tmpList.begin(); it != tmpList.end(); ++it)
+            if (_instance == null)
             {
-                DoCommand(*it);
+                _instance= new CommandHistory();
+            }
+            return _instance;
+        }
+        /// <summary>
+        /// This clean up the class.
+        /// </summary>
+        public void Clear()
+        {
+            redoList.Clear();
+            undoList.Clear();
+            _instance = null;
+        }
+        /// <summary>
+        ///  Add the current command to the history with the parameters.
+        /// <param name="commandWrapper">Current command coupled with parameters to be added to the history</param>
+        /// </summary>
+        public void AddHistory(CommandWrapper commandWrapper)
+        {
+            historyList.Add(commandWrapper);
+        }
+        /// <summary>
+        ///  Repeat all the commands except the last one.
+        /// </summary>
+        public void Repeat()
+        {
+            IList<CommandWrapper> tmpList = new List<CommandWrapper>();
+            redoList.Clear();
+            undoList.Clear();
+            foreach (var cmd in historyList)
+            {
+                tmpList.Add(cmd);
+            }
+            
+            tmpList.RemoveAt(tmpList.Count - 1);
+            foreach (var cmd in tmpList)
+            {
+                DoCommand(cmd);
             }
         }
-        void CommandHistory::DoCommand(Command* c)
+        /// <summary>
+        /// Check if a command can be executed, a
+        /// </summary>
+        /// <param name="commandWrapper"></param>
+        private void DoCommand(CommandWrapper commandWrapper)
         {
-            ClearList(&_redoList);
-            if (c->Execute())
+            ICommand command = commandWrapper.Command;
+            object param = commandWrapper.Parameters;
+            redoList.Clear();
+            if (command.CanExecute(param))
             {
-                AddUndo(c);
-                //AddHistory(c);	
+                command.Execute(param);
+                AddUndo(commandWrapper);
             }
         }
-        bool CommandHistory::isDirty() const
+        /// <summary>
+        /// This returns if the code is dirty or not.
+        /// </summary>
+        /// <returns></returns>
+        public bool IsDirty()
         {
-            return (_cleanCount != 0);
+            return (_cleanUpCount != 0);
         }
-        Command* CommandHistory::getLastRedoCommand() const
+       
+        public CommandWrapper GetLastRedoCommand()
         {
-            return _redoList.back();
+            return redoList[redoList.Count - 1];
         }
-        Command* CommandHistory::getLastUndoCommand() const
-        {
-            return _undoList.back();
-        }
-        bool CommandHistory::CanUndo() const
-        {
-            return (_undoList.size()>0);
-        }
-        bool CommandHistory::CanRedo() const
-        {
-            return (_redoList.size()>0);
-        }
-        void CommandHistory::Undo()
-        {
-            bool state = false;
 
+        public CommandWrapper GetLastUndoCommand()
+        {
+            return undoList[undoList.Count - 1];
+        }
+
+        public bool CanUndo()
+        {
+            return (undoList.Count > 0);
+        }
+
+        public bool CanRedo()
+        {
+            return (redoList.Count > 0);
+        }
+
+        public void Undo()
+        {
             if (CanUndo())
             {
-                _cleanCount--;
-                Command* c = _undoList.back();
-                _undoList.pop_back();
-                if ((c) && (c->Unexecute()))
+                _cleanUpCount--;
+                CommandWrapper tmpCommandWrapper = GetTailAndRemove(undoList);
+                if (tmpCommandWrapper.Command.UnExecute())
                 {
-                    AddRedo(c);
-                    //AddHistory(c);
+                    AddRedo(tmpCommandWrapper);
                 }
-                else
-                    secure::safe_delete<Command>(c);
-
             }
         }
 
-        void CommandHistory::Redo()
+        public void Redo()
         {
             if (CanRedo())
             {
-                _cleanCount++;
-                Command* c = _redoList.back();
-                _redoList.pop_back();
-                if (c->Execute())
+                _cleanUpCount++;
+                CommandWrapper tmpCommandWrapper = GetTailAndRemove(redoList);
+
+                if (tmpCommandWrapper.Command.CanExecute(tmpCommandWrapper.Parameters))
                 {
-                    AddUndo(c);
-                    //AddHistory(c);
+                    AddUndo(tmpCommandWrapper);
                 }
-                else
-                    secure::safe_delete<Command>(c);
-            }
-
-        }
-// cleanup
-        void CommandHistory::Clear()
-        {
-            if (instance != 0)
-            {
-                delete instance;
             }
         }
-        void CommandHistory::EraseList(CommandList* pList)
+
+        private void AddUndo(CommandWrapper commandWrapper)
         {
-            pList->erase(pList->begin(), pList->end());
-        }
-        void CommandHistory::ClearList(CommandList* pList)
-        {
-            command_iterator it;
-            for (it = pList->begin(); it != pList->end(); it++)
-            {
-                secure::safe_delete<Command>(*it);
-            }
-            pList->erase(pList->begin(), pList->end());
-        }
-// cleanup me
-        void CommandHistory::setClean()
-        {
-            _cleanCount = 0;
-        }
-        void CommandHistory::ClearRedoList()
-        {
-            ClearList(&_redoList);
-        }
-        void CommandHistory::ClearUndoList()
-        {
-            ClearList(&_undoList);
+             undoList.Add(commandWrapper);
+            _cleanUpCount = ((_cleanUpCount < 0) && (redoList.Count > 0)) ? undoList.Count + redoList.Count + 1 : _cleanUpCount++;
         }
 
-        CommandHistory::~CommandHistory(void)
-        {	
-
-            setClean();
-
-            ClearList(&_history);
-
-            ClearRedoList();
-
-            ClearUndoList();
-        }
-// implementations
-        void CommandHistory::AddUndo(Command* pCommand)
+        private void AddRedo(CommandWrapper tmpCommandWrapper)
         {
-            if (_undoList.size() >= MAX_UNDO_COMMAND)
-            {
-                delete _undoList.front();
-                _undoList.pop_front();
-            }
-            _undoList.push_back(pCommand);
-            if (_cleanCount < 0 && _redoList.size() > 0)
-                _cleanCount = (int)_undoList.size() + (int)_redoList.size() + 1;
-            else
-                _cleanCount++;
+            redoList.Add(tmpCommandWrapper);
         }
-        void CommandHistory::AddRedo(Command* pCommand)
+        private CommandWrapper GetTailAndRemove(IList<CommandWrapper> commandWrapperList)
         {
-            _redoList.push_back(pCommand);
+            CommandWrapper tmpCommandWrapper = commandWrapperList[commandWrapperList.Count - 1];
+            commandWrapperList.RemoveAt(commandWrapperList.Count - 1);
+            return tmpCommandWrapper;
         }
-        private static CommandHistory _instance = null;
-        private int _cleanCount = 0;
-        public CommandHistory GetInstance()
-        {
-            if (_instance == null)
-                _instance = new CommandHistory();
-            return _instance;
-        }
-        void CommandHistory.AddHistory(Ab* c)
-        {
-            if (_history.size() > MAX_UNDO_COMMAND * 2)
-                ClearList(&_history);
-            _history.push_back(c);
-        }
-         */
-
     }
 
 }
