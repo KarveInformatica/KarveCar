0ab20655f2c1b50232c54e314222e18eda4da331
Jordi Fernández <jordi.fernandez@karveinformatica.com>
2017-07-26 12:00:58 +0200

    karve commit.

---
+++ src/Model/Generic/IDataGridRowChange.cs
@@ -0,0 +1,11 @@
+﻿using static KarveCar.Model.Generic.RecopilatorioEnumerations;
+
+namespace KarveCar.Model.Generic
+{
+    public interface IDataGridRowChange
+    {
+        string UltimaModificacion { get; set; }
+        string Usuario { get; set; }
+        EControlCambioDataGrid ControlCambioDataGrid { get; set; }
+    }
+}
