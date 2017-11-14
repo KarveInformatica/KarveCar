065a456097e83d916303078ed1f0330f5198183f
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2017-11-14 23:53:06 +0100

    Changed control and material design

---
+++ src/Controls/KarveControls/ItemsControl/DataPickerItemsControl.xaml.cs
@@ -0,0 +1,168 @@
+﻿using KarveControls.Generic;
+using Prism.Commands;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KarveControls.ItemsControl
+{
+    /// <summary>
+    /// Interaction logic for DataPickerItemsControl.xaml
+    /// </summary>
+    
+    public partial class DataPickerItemsControl : UserControl
+    {
+        private DelegateCommand<object> _localCommand;
+
+        /// <summary>
+        ///  This expose the change action dependency property.
+        /// </summary>
+        public static DependencyProperty ChangeActionDependencyProperty
+        = DependencyProperty.Register(
+            "ChangeActionCommand",
+            typeof(ICommand),
+            typeof(DataPickerItemsControl));
+        /// <summary>
+        ///  localization of strings.
+        /// </summary>
+        public ICommand ChangeActionCommand
+        {
+            get { return (ICommand)GetValue(ChangeActionDependencyProperty); }
+            set { SetValue(ChangeActionDependencyProperty, (ICommand)value); }
+        }
+        /// <summary>
+        ///  This trigger a command during a change.
+        /// </summary>
+        /// <param name="obj">parameters of the change</param>
+        private void OnChangeField(object obj)
+        {
+            ICommand value = ChangeActionCommand;
+            if (value != null)
+                value.Execute(obj);
+        }
+        /// <summary>
+        /// Localization stuff.
+        /// </summary>
+        public static DependencyProperty LabelEnumerable = DependencyProperty.Register("Localization",
+            typeof(IEnumerable<string>),
+            typeof(DataPickerItemsControl),
+            new PropertyMetadata(new List<string>(), OnLabelSourceChanged));
+        /// <summary>
+        /// Label source changed.
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        private static void OnLabelSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DataPickerItemsControl control = d as DataPickerItemsControl;
+            control.OnLocaleLabelChanged(e);
+        }
+
+        private void OnLocaleLabelChanged(DependencyPropertyChangedEventArgs e)
+        {
+            List<string> localizationStrings = e.NewValue as List<string>;
+            if (localizationStrings != null)
+            {
+                ComponentFiller filler = new ComponentFiller();
+                List<ComponentFiller.UiMetaObject> listOfObjects = filler.GetCollectionProperties(DataSource);
+
+                for (int i = 0; i < localizationStrings.Count; i++)
+                {
+                    if (i < listOfObjects.Count)
+                    {
+                        listOfObjects[i].LabelText = localizationStrings[i];
+                    }
+                }
+                for (int i = 0; i < listOfObjects.Count; i++)
+                {
+                    listOfObjects[i].ChangedItem = _localCommand;
+                }
+
+
+            }
+        }
+
+        /// <summary>
+        ///  localization of strings.
+        /// </summary>
+        public IEnumerable<string> Localization
+        {
+            get { return (IEnumerable<string>)GetValue(LabelEnumerable); }
+            set { SetValue(LabelEnumerable, (IEnumerable)value); }
+        }
+        /// <summary>
+        /// Data Source Dependency Property
+        /// </summary>
+        public static DependencyProperty DataSourceDependencyProperty
+            = DependencyProperty.Register(
+                "DataSource",
+                typeof(object),
+                typeof(DataPickerItemsControl),
+                new PropertyMetadata(null, OnDataSourceChanged));
+        /// <summary>
+        ///  Data source property handy way to associate a data field to a value.
+        /// </summary>
+        public object DataSource
+        {
+            get { return GetValue(DataSourceDependencyProperty); }
+            set { SetValue(DataSourceDependencyProperty, value); }
+        }
+
+        private static void OnDataSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DataPickerItemsControl control = d as DataPickerItemsControl;
+            if (control != null)
+            {
+                control.OnDataSourceChanged(e);
+            }
+        }
+        /// <summary>
+        ///  DataSourceChanged
+        /// </summary>
+        /// <param name="d"></param>
+        private void OnDataSourceChanged(DependencyPropertyChangedEventArgs e)
+        {
+            object value = e.NewValue;
+            ComponentFiller filler = new ComponentFiller();
+            List<ComponentFiller.UiMetaObject> listOfObjects = filler.GetCollectionProperties(value);
+            int count = listOfObjects.Count<ComponentFiller.UiMetaObject>();
+            IList<string> localizationStrings = Localization as List<string>;
+            int secondCount = localizationStrings.Count<string>();
+            // i am sure that the component filler gives us a list of objects.
+            for (int i = 0; i < count; i++)
+            {
+                listOfObjects[i].ChangedItem = _localCommand;
+            }
+            if (count == secondCount)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    listOfObjects[i].LabelText = localizationStrings[i];
+                }
+            }
+            
+            this.DataControl.ItemsSource = listOfObjects;
+        }
+
+        /// <summary>
+        ///  Items control for multiple data picker with a list of label and bindings.
+        /// </summary>
+        public DataPickerItemsControl()
+        {
+            _localCommand = new DelegateCommand<object>(OnChangeField);
+            InitializeComponent();
+        }
+    }
+}
