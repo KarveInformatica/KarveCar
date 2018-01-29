b76c1cb65e8bc4adbe86bb265c2a595b6bca797a
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2018-01-30 00:30:33 +0100

    Snapshot R2

---
+++ src/Modules/DALModule/KarveDataServices/DataTransferObject/CurrentSituationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarveDataServices.DataTransferObject
+{
+    /// <summary>
+    ///  Public current situation dto.
+    /// </summary>
+    public class CurrentSituationDto: BaseDto
+    {
+        /// <summary>
+        ///  code
+        /// </summary>
+        public byte Code { set;get; }
+        /// <summary>
+        ///  name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
